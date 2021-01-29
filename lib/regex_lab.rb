1e403cd2e7794ce39123a7dce81994075acf9b87
Kristyn Little <kristynlittle@frontier.com>
2021-01-29 05:02:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-return text.match(/^[A-Z].*[.?!]$/)
+return text.match(/^[A-Z].*[.?!]$/) != nil
 end
 
 def valid_phone_number?(phone)
