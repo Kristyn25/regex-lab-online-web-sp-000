de9c9655c93bc27ca65afee387c2cf8a26ea5cf4
Kristyn Little <kristynlittle@frontier.com>
2021-01-29 04:12:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ return word.match(/^[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-return text.scan(/^\w+un+ing/)
+return text.scan(/^\w+un||ing/)
 end
 
 def words_five_letters_long(text)
